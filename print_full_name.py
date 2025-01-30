a7a4d931059c1da97ba85a58e1079878c4f3daf3
Programming-Sai <151084650+Programming-Sai@users.noreply.github.com>
2025-01-30 20:23:47 +0000

    Add files via upload

---
+++ print_full_name.py
@@ -0,0 +1,4 @@
+def print_full_name(first, last):
+    return f"Hello {first} {last}! You just delved into python."
+
+print(print_full_name("Ross", "Taylor"))
---
+++ split_join.py
@@ -0,0 +1,5 @@
+def split_and_join(line):
+    return "-".join(line.split(" "))
+
+
+print(split_and_join("this is a string"))
